60f272be73fdde5a8ded427ccf4172e7a25af13b
Artem_Volchugin <volchugin.artem@gmail.com>
2025-04-22 20:08:25 +0300

    add file for work with db

---
+++ SQL.py
@@ -0,0 +1,25 @@
+import sqlite3
+print('Выберите режим: select - S, update - U')
+mode = input()
+print('Введите слово:')
+word = input()
+length = len(word)
+connection = sqlite3.connect('words.db')
+cursor = connection.cursor()
+
+def select(a,b):
+    cursor.execute(f'INSERT INTO Words (word, length) VALUES (?, ?)', (a, b))
+
+def update(a,b):
+    cursor.execute('UPDATE Words SET word = ? WHERE id = ?', (a, b))
+
+
+if mode == 'S':
+    select(word,length)
+elif mode == 'U':
+    update(word,length)
+else:
+    print('Выбран неверный режим работы, попробуйте снова')
+
+connection.commit()
+connection.close()
